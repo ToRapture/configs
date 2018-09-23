29a64925984f33a44ef2af1d88a4b1775d69f55d
ToRapture <torapture@gmail.com>
2018-09-23 19:04:52 +0800

    git

--- git/git_config.sh
+++ git/git_config.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 git config --global user.email torapture@gmail.com
 git config --global user.name ToRapture
 git config --global core.editor vim
@@ -5,4 +7,3 @@ git config --global core.quotepath false
 git config --global push.default simple
 git config --global diff.tool vimdiff  
 git config --global difftool.prompt No
-
