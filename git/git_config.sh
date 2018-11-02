ec50b4b99ea7ffcbf5fba5ea9d0d7e5fd0f8a0f8
ToRapture <torapture@gmail.com>
2018-11-02 13:31:08 +0800

    git config

--- git/git_config.sh
+++ git/git_config.sh
@@ -7,3 +7,5 @@ git config --global core.quotepath false
 git config --global push.default simple
 git config --global diff.tool vimdiff  
 git config --global difftool.prompt No
+git config --global log.date iso
+git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%ci) %C(bold blue)<%an>%Creset' --abbrev-commit --"
