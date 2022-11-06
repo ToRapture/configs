395e08bc31e217755a1aa8bda036d17414efed01
ToRapture <torapture@gmail.com>
2022-11-06 18:10:42 +0900

    fix empty profile and support mac

--- powershell/setup.ps1
+++ powershell/setup.ps1
@@ -7,28 +7,35 @@ Invoke-Expression $user_profile
 
 "@
 
-if (!(Get-Content $PROFILE -Raw).Contains($user_profile)) {
+$profile_content = Get-Content $PROFILE -Raw
+if ($null -eq $profile_content -or !$profile_content.Contains($user_profile)) {
     $head_content + (Get-Content $PROFILE -Raw) | Set-Content $PROFILE
 }
 
-# Install oh-my-posh
-winget install JanDeDobbeleer.OhMyPosh -s winget
 
-# Install scoop
-if (![bool](Get-Command -Name 'scoop' -ErrorAction SilentlyContinue)) {
-    # Copied from https://scoop.sh/
-    Set-ExecutionPolicy RemoteSigned -Scope CurrentUser # Optional: Needed to run a remote script the first time
-    Invoke-RestMethod get.scoop.sh | Invoke-Expression
+if ($IsWindows) {
+    # Install oh-my-posh
+    winget install JanDeDobbeleer.OhMyPosh -s winget
+
+    # Install scoop
+    if (![bool](Get-Command -Name 'scoop' -ErrorAction SilentlyContinue)) {
+        # Copied from https://scoop.sh/
+        Set-ExecutionPolicy RemoteSigned -Scope CurrentUser # Optional: Needed to run a remote script the first time
+        Invoke-RestMethod get.scoop.sh | Invoke-Expression
+    }
+
+    # Install scoop commands
+    scoop install curl
+    scoop install sudo
+    scoop install jq
+    scoop install less
+    scoop install grep
+    scoop install fzf
+    scoop install ag
 }
-
-# Install scoop commands
-scoop install curl
-scoop install sudo
-scoop install jq
-scoop install less
-scoop install grep
-scoop install fzf
-scoop install ag
+else {
+    brew install jandedobbeleer/oh-my-posh/oh-my-posh
+}
 
 # Install Modules
 sudo Install-Module posh-git -Force
